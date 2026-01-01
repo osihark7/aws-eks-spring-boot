84fac68d45b56be547dcd2d7b0b8aa2c842b0dcf
osihar <osihark73@gmail.com>
2026-01-02 02:23:24 +0530

    Inital commit

---
+++ src/main/java/com/aws_eks_spring_boot/aws_eks_spring_boot/AwsEksSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.aws_eks_spring_boot.aws_eks_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEksSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEksSpringBootApplication.class, args);
+		
+		System.out.println("Welcome to learning aws -eks-deployment....");
+		
+	}
+
+}
---
+++ src/test/java/com/aws_eks_spring_boot/aws_eks_spring_boot/AwsEksSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws_eks_spring_boot.aws_eks_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEksSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
